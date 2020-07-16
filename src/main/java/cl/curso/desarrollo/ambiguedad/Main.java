69ef8421d565c6a03d00f47c43709e528627debb
Juan Daniel Muñoz Queupul <munoz.queupul.daniel.juan@gmail.com>
2020-07-16 18:52:03 -0400

    11. Expresión lambda como retorno de métodos

---
+++ src/main/java/cl/curso/desarrollo/ambiguedad/Main.java
@@ -0,0 +1,16 @@
+package cl.curso.desarrollo.ambiguedad;
+
+public class Main {
+	
+
+	public static void main(String[] args) {
+
+		create().calculate(100, 20);
+
+	}
+	
+	private static CalculadoraLong create() {
+		return (x, y) -> System.out.println(x/y);
+	}
+
+}
